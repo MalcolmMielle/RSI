3acb8b5b1ff5ab57a1101eadedf68a2526732667
Malcolm Mielle <malcolm.mielle@protonmail.com>
2016-05-18 15:40:34 +0200

    Added Kmean as a way to reduce noise :D

--- Test/test_watershed.cpp
+++ Test/test_watershed.cpp
@@ -12,6 +12,7 @@
 
 #include "WaterShed.hpp"
 #include "FuzzyOpening.hpp"
+#include "Kmean.hpp"
 
 
 BOOST_AUTO_TEST_CASE(trying)
@@ -40,20 +41,44 @@ BOOST_AUTO_TEST_CASE(trying)
 	
 	cv::Mat out_tmp;
 // 	cv::GaussianBlur(out, out_tmp, cv::Size(3, 3), 10);
-	cv::medianBlur(out, out_tmp, 11);
+// 	cv::medianBlur(out, out_tmp, 11);
 // 	cv::bilateralFilter(out, out_tmp, 5, 10, 0);
+
+// 	cv::Mat element = cv::getStructuringElement(cv::MORPH_RECT, cv::Size( 11, 11 ), cv::Point( -1, -1 ) );
+	/// Apply the specified morphology operation
+// 	cv::morphologyEx( out, out_tmp, cv::MORPH_CLOSE, element);
+	
+// 	std::cout << "FILTER " << std::endl << out << std::endl;
+	
+// 	cv::pyrDown(out, out_tmp, cv::Size(out.cols/2, out.rows/2 ) );
+// 	cv::pyrUp(out_tmp, out_tmp, cv::Size(out_tmp.cols*2, out_tmp.rows*2 ) );
+	
+// 	cv::resize(out, out_tmp, cv::Size(out.cols/2, out.rows/2 ), 0, 0, cv::INTER_NEAREST);
+	
+	
 // 	cv::GaussianBlur(out, out_tmp, cv::Size(0, 0), 3);
 // 	cv::addWeighted(out, 1.5, out_tmp, -0.5, 0, out_tmp);
 	
-// 	std::cout << "FILTER " << std::endl << out << std::endl;
+// 	cv::resize(out_tmp, out_tmp, cv::Size(out_tmp.cols*2, out_tmp.rows*2 ), 0, 0, cv::INTER_NEAREST);
+	
+// 	std::cout << "out_tmp" << out_tmp << std::endl;
+	
+	
+	AASS::RSI::Kmeans kmeans;
+	
+	kmeans.kmeansColor(out, out_tmp);
+	
+	
 	
 	AASS::RSI::Watershed watershed;
 	std::cout << "WHATERSHED" << std::endl;
 	watershed.watershed(out_tmp);
 	
-// 	watershed.print();
+	watershed.print();
 	
-	cv::imshow("out_tmp", out);
+	std::cout << "Final zone number " << watershed.size() <<std::endl;
+	
+	cv::imshow("out_tmp", out_tmp);
 	cv::waitKey(0);
 	watershed.drawAllZones(out_tmp, 0);
 	
---
+++ include/Kmean.hpp
@@ -0,0 +1,100 @@
+#ifndef RSI_KMEANS_18052016
+#define RSI_KMEANS_18052016
+
+#include <opencv2/opencv.hpp>
+#include "Utils.hpp"
+
+namespace AASS{
+	namespace RSI{
+		
+		class Kmeans{
+			
+		public:
+			Kmeans(){}
+			void kmeansColor(cv::Mat& in, cv::Mat& dest){
+				cv::imshow("input", in);
+				cv::Mat input;
+				std::cout << "TYPE " << type2str(in.type()) << std::endl;
+				in.convertTo(input, CV_8UC1);
+				cv::Mat bestLabels, centers;
+				
+				
+				cv::Mat p = cv::Mat::zeros(input.cols*input.rows, 3, CV_32F);
+				std::vector<cv::Mat> bgr;
+				cv::split(input, bgr);
+// 				SLOWWWWW
+				for(int i = 0 ; i < input.cols*input.rows ; i++) {
+					p.at<float>(i,0) = (i/input.cols) / input.rows; //Needs to be zero because it fails when I seem to put the good values...
+					p.at<float>(i,1) = (i%input.cols) / input.cols;
+					p.at<float>(i,2) =  bgr[0].data[i] / 255.0;
+				}
+				
+// 				for(int row = 0 ; row < input.rows ; row++){
+// 					uchar* pointer = input.ptr<uchar>(row); //point to each row
+// 					for(int col = 0 ; col < input.cols ; col++){
+// // 						std::cout << "value : " << (float) pointer[col] << " at " << (row * input.cols) + col << std::endl;
+// 						p.at<float>( (row * input.cols) + col, 0) = (float) row / input.rows;
+// 						p.at<float>( (row * input.cols) + col, 1) = (float) col / input.cols;
+// 						p.at<float>( (row * input.cols) + col, 2) = pointer[col] / 255.0;
+// 					}
+// 				}
+				
+// 				std::cout << "second p " << std::endl << p << std::endl;
+// 				exit(0);
+// 				std::cout << "DATA "<< std::endl << p << std::endl;
+				int K = 4;
+				cv::kmeans(p, K, bestLabels, cv::TermCriteria( CV_TERMCRIT_EPS+CV_TERMCRIT_ITER, 10, 1.0), 3, cv::KMEANS_PP_CENTERS, centers);
+				
+				int colors[K];
+				for(int i=0; i<K; i++) {
+					colors[i] = 255/(i+1);
+				}
+				// i think there is a better way to do this mayebe some Mat::reshape?
+				dest = cv::Mat::zeros(input.rows, input.cols, CV_8U);
+// 				for(int i=0; i<input.cols*input.rows; i++) {
+// 					dest.at<float>(i/input.cols, i%input.cols) = (float)(colors[bestLabels.at<int>(0,i)]);
+// 				//      cout << bestLabels.at<int>(0,i) << " " << 
+// 				//              colors[bestLabels.at<int>(0,i)] << " " << 
+// 				//              clustered.at<float>(i/src.cols, i%src.cols) << " " <<
+// 				//              endl;
+// 				}
+				
+				std::cout << "TYPE " << type2str(centers.type()) << " size " << centers.rows << " " << centers.cols << std::endl;
+// 				
+				uint32_t* pointer_label = bestLabels.ptr<uint32_t>(0); //point to each row
+				
+				for(int row = 0 ; row < input.rows ; row++){
+					uchar* pointer = dest.ptr<uchar>(row); //point to each row
+					
+					for(int col = 0 ; col < input.cols ; col++){
+// 						std::cout << "AT " << pointer_label[(row * input.cols) + col] << std::endl;
+						float* pointer_center = centers.ptr<float>(pointer_label[(row * input.cols) + col]);
+						
+// 						std::cout << "Center : " << pointer_center[0] << " " << pointer_center[1] << " " << pointer_center[2] * 255 << " " << std::endl;
+// 						std::cout << "Value in base " << (int) in.at<uchar>(pointer_center[0] * in.rows, pointer_center[1] * in.cols) << std::endl;
+					
+// 						pointer[col] = (uchar)(colors[ pointer_label[(row * input.cols) + col] ]);
+// 						not working
+// 						pointer[col] = (uchar)(pointer_center[2] * 255);
+						pointer[col] = (int) ( ((float)pointer_center[2] * 255) );
+					}				
+				} 
+				
+// 				for(int row = 0 ; row < centers.rows ; row++){
+// 					uchar* pointer = dest.ptr<uchar>(row); //point to each row
+// 					float* pointer_center = centers.ptr<float>(row);
+// // 						std::cout << "AT " << pointer_label[(row * input.cols) + col] << std::endl;
+// 					std::cout << "Center : " << pointer_center[0] * input.rows << " " << pointer_center[1] * input.cols << " " << (float) ((float)pointer_center[2] * 255) << " color " << (int) (in.at<uchar>(pointer_center[0] * input.rows, pointer_center[1] * input.cols)) << std::endl;
+// 						
+// 					
+// 				}
+				
+// 				std::cout << centers << std::endl;
+// 				cv::normalize(dest, dest, 0, 255, cv::NORM_MINMAX, CV_8U);
+// 				dest.convertTo(dest, CV_8U);
+			}
+			
+		};
+	}
+}
+#endif
