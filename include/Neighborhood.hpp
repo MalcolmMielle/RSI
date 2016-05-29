3874afa05c5f18c51a77c5f01d9ed6509931d0c1
Malcolm Mielle <malcolm.mielle@protonmail.com>
2016-05-29 12:58:14 +0200

    Added Neighbor

--- include/Neighborhood.hpp
+++ include/Neighborhood.hpp
@@ -7,23 +7,49 @@ namespace AASS{
 	
 	namespace RSI{
 		
+		class Neighbor{
+		protected:
+			std::pair <Zone, EdgeElement> _element;
+			std::pair <GraphZone::VertexZone, GraphZone::EdgeZone> _accessor;
+		public:
+			Neighbor(const GraphZone::VertexZone vertex, const GraphZone::EdgeZone& edge, const Zone& zone, const EdgeElement& edgeelement) : _element(std::pair <Zone, EdgeElement>(zone, edgeelement)), _accessor(std::pair <GraphZone::VertexZone, GraphZone::EdgeZone>(vertex, edge)){};
+			
+			
+			GraphZone::VertexZone getVertex(){return _accessor.first;}
+			Zone getVertexElement(){return _element.first;}
+			GraphZone::Edge getEdge(){return _accessor.second;}
+			EdgeElement getEdgeElement(){return _element.second;}
+			
+			const GraphZone::VertexZone getVertex() const {return _accessor.first;}
+			const Zone getVertexElement() const {return _element.first;}
+			const GraphZone::Edge getEdge() const {return _accessor.second;}
+			const EdgeElement getEdgeElement() const {return _element.second;}
+			
+		};
+		
+		
 		class Neighborhood : std::deque{
 		protected:
-			std::deque< std::pair <GraphZone::VertexZone, GraphZone::EdgeZone> > _neighborhood;
+			std::deque< Neighbor > _neighborhood;
 			GraphZone::VertexZone _center;
+			Zone _center_zone;
 			
 		public:
 			Neghborhood(){};
 			
-			void push_back(const std::pair <GraphZone::VertexZone, GraphZone::EdgeZone>& v){ _neighborhood.push_back(v);}
-			void push_front(const std::pair <GraphZone::VertexZone, GraphZone::EdgeZone>& v){ _neighborhood.push_front(v);}
-			void push_back(const GraphZone::VertexZone v, const GraphZone::EdgeZone& edge){ _neighborhood.push_back(std::pair<GraphZone::VertexZone, GraphZone::EdgeZone>(v, edge));}
-			void push_front(const GraphZone::VertexZone v, const GraphZone::EdgeZone& edge){ _neighborhood.push_front(std::pair<GraphZone::VertexZone, GraphZone::EdgeZone>(v, edge));}
+			void push_back(const GraphZone::VertexZone v, const GraphZone::EdgeZone& edge, const Zone& zone, const EdgeElement& edgeelement){ 
+				Neighbor el(v, edge, zone, edgeelement); _neighborhood.push_back(el);
+			}
+			
+			void push_front(const GraphZone::VertexZone v, const GraphZone::EdgeZone& edge, const Zone& zone, const EdgeElement& edgeelement){
+				Neighbor el(v, edge, zone, edgeelement);
+				_neighborhood.push_front(el);
+			}
 			
 			void pop_back(){_neighborhood.pop_back();}
 			void pop_front(){_neighborhood.pop_front();}
 			
-			void setCenter(const GraphZone::VertexZone& v){_center = v;}
+			void setCenter(const GraphZone::VertexZone& v, const Zone& zone){_center = v; _center_zone = zone;}
 			const GraphZone& getCenter() const {return _center;}
 			GraphZone getCenter(){return _center;}
 			
@@ -31,11 +57,11 @@ namespace AASS{
 		};
 		
 		
-		inline double editDistanceNeighborhood(const Neighborhood& n1, const Neighborhood& n2){
-			
-			
-		}
 		
+		//TODO
+		inline double compareNeighbor(const Neighbor& n1, const Neighbor& n2){
+			
+		};
 		
 		
 	}
