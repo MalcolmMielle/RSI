bbea1c78f9c02b1d737aa0b55c838a7399b774a4
Malcolm Mielle <malcolm.mielle@protonmail.com>
2017-05-10 23:21:58 +0200

    cmake

--- rsi-config.cmake
+++ rsi-config.cmake
@@ -1,4 +1,4 @@
 
 get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
-get_filename_component(RSI_INCLUDE_DIRS "${SELF_DIR}/../../include/RSI" ABSOLUTE)
-set(RSI_LIBRARIES "${SELF_DIR}/libRSILIB.a")
+get_filename_component(RSI_INCLUDE_DIRS "${SELF_DIR}/../../include/RSI;${SELF_DIR}/../../include/RSI/hungarian" ABSOLUTE)
+set(RSI_LIBRARIES "${SELF_DIR}/libRSILIB.a;${SELF_DIR}/libhungarian_lib.a")
