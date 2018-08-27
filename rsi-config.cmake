1e308fc0740bf9557d17f514eeb0461848058c5d
Malcolm Mielle <malcolm.mielle@protonmail.com>
2018-08-27 20:41:27 +0200

    config: depend maoris
    Added the dependency on maoris, in the config file
    
    Malcolm

--- rsi-config.cmake
+++ rsi-config.cmake
@@ -1,4 +1,5 @@
+find_package( maoris REQUIRED )
 
 get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
-get_filename_component(RSI_INCLUDE_DIRS "${SELF_DIR}/../../include/RSI;${SELF_DIR}/../../include/RSI/hungarian" ABSOLUTE)
-set(RSI_LIBRARIES "${SELF_DIR}/libRSILIB.a;${SELF_DIR}/libhungarian_lib.a")
+get_filename_component(RSI_INCLUDE_DIRS "${SELF_DIR}/../../include/RSI;${SELF_DIR}/../../include/RSI/hungarian;${maoris_INCLUDE_DIRS}" ABSOLUTE)
+set(RSI_LIBRARIES "${SELF_DIR}/libRSILIB.a;${SELF_DIR}/libhungarian_lib.a;${maoris_LIBRARIES}")
